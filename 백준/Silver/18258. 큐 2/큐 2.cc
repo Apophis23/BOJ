1a844ed707c4170ce47d3c541f706307beea5768
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-03-23 20:50:49 +0900

    [Silver IV] Title: 큐 2, Time: 396 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+
+	queue<int>que;
+	string str;
+	int num = 0, number = 0;
+	cin >> number;
+	for (int i = 0; i < number; i++) {
+		cin >> str;
+
+		if (str == "push") {
+			cin >> num;
+			que.push(num);
+		}
+
+		else if (str == "pop") {
+			if (que.empty() == true) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << que.front() << "\n";
+				que.pop();
+			}
+		}
+
+		else if (str == "size") {
+			cout << que.size() << "\n";
+		}
+
+		else if (str == "empty") {
+			if (que.empty() == true) {
+				cout << 1 << "\n";
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+
+		else if (str == "front") {
+			if (que.empty() == true) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << que.front() << "\n";
+			}
+		}
+
+		else if (str == "back") {
+			if (que.empty() == true) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << que.back() << "\n";
+			}
+		}
+	}
+}
