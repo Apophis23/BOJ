264808369289526627a8f9dc5c58ac1baaac421b
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-03-23 20:54:54 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int num = 0, number = 0;
+	for (int i = 0; i < num * 2; i++) {
+		cin >> number;
+	}
+	cout << "비와이";
+}
