cb9e242a3d882d4366f587f3948e934706d39a70
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-03-23 20:54:15 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 244 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base:: sync_with_stdio(false);
+	int a;
+	int b;
+	int c;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		cin >> b >> c;
+		cout << b + c << "\n";
+	}
+}
