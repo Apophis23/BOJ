ef7c5a69398efe4b44956b307c06a71de448e119
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-03-23 20:52:21 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int n = 0, m = 0;
+int arr[9] = { 0, };
+bool check[9] = { 0, };
+
+void func(int c,int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int j = c; j <= n; j++) {
+		arr[k] = j;
+	    func(j, k + 1);
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	func(1, 0);
+}
