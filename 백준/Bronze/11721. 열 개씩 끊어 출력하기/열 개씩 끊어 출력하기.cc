ec8800973236453169fa414fd246bc067da7a4f3
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-03-28 01:48:23 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string str;
+	int count = 1;
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		if (i != 0 && i % 10 == 0) {
+			cout << "\n";
+		}
+		cout << str[i];
+	}
+}
