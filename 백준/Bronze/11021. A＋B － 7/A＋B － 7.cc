94a27913406be0a4e3aec893fb172500a02a82c9
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-07-14 14:39:36 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base:: sync_with_stdio(false);
+	int a;
+	int b;
+	int c;
+	cin >> a;
+	for (int i = 1; i < a + 1; i++) {
+		cin >> b >> c;
+		cout << "Case #" << i << ": " << b + c << "\n";
+	}
+}
