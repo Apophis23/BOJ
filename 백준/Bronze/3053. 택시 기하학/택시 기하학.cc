7151bb59af935d8e6e96000da89c7532dcb7987b
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-07-14 14:30:56 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+	double num = 0, pie = 3.1415926535897932, taxi = 0, uclid = 0;
+	cin >> num;
+	cout << fixed;
+	cout.precision(6);
+	cout << num * num*pie << "\n" << 2 * num*num;
+}
