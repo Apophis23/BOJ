047d5cfe665b456381ec97a1f0d1966f27336ac2
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-07-14 14:30:42 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int num=0, average=0, result = 0;
+	cin >> num >> average;
+	result = 2 * average - num;
+	cout << result;
+}
