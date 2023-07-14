0cd0c154d10265d9d7ae14c58203ed4542262414
Apophis23 <116174933+Apophis23@users.noreply.github.com>
2023-07-14 14:37:16 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	int b;
+	int c;
+	cin >> a >> b >> c;
+	if (b >= a&&a >= c) {
+		cout << a << endl;
+	}
+	else if (c >= a&&a >= b) {
+		cout << a << endl;
+	}
+	else if (a >= b&&b >= c) {
+		cout << b << endl;
+	}
+	else if (c >= b&&b >= a) {
+		cout << b << endl;
+	}
+	else if (a >= c&&c >= b) {
+		cout << c << endl;
+	}
+	else {
+		cout << c << endl;
+	}
+}
